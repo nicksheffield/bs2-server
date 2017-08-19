75bfd29eb4c3be9afd8fd5f81b9311bacc4891e2
Nick Sheffield <numbereft@gmail.com>
2017-08-19 17:21:17 +1200

    Added status field to units

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -14,12 +14,25 @@ class Unit extends Model
 	protected $fillable = [
 		'serial_number', 'asset_number', 'unit_number', 'product_id', 'notes'
 	];
+
+	protected $appends = [
+		'status'
+	];
 	
 	public function booking_products() {
-		return $this->hasMany('App\Models\BookingProduct');
+		return $this->hasMany('App\Models\BookingProduct')->has('booking');
 	}
 	
 	public function product() {
 		return $this->belongsTo('App\Models\Product', 'product_id');
+	}
+
+	public function getStatusAttribute() {
+		foreach ($this->booking_products as $bp) {
+			if (!$bp->booking) continue;
+			if (!$bp->returned_at) return 'Issued';
+		}
+
+		return 'In Stock';
 	}
 }
